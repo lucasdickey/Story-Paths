135b212c343b2b106c9b4c08917b199f0dc97e59
lucas-azibo <lucas@azibo.com>
2023-07-01 18:25:04 -0700

    Added two primary components
    Added input form & component to display output on call to OpenAI service

---
+++ StoryPaths/components/StoryDisplay.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const StoryDisplay = ({ story }) => {
+    return (
+      <div>
+        {story ? (
+          <div className="story">
+            <p>{story}</p>
+          </div>
+        ) : (
+          <p>Enter the details and click the button to generate your story!</p>
+        )}
+      </div>
+    );
+  };
+  
+  // TODO: Take output story which is already in paragraphs, break into separate objects, and allow for inserting images from DALL-E sporadically between the paragraphs
+  
+  export default StoryDisplay;
